3fe2d8c6d3feeb9d0e380f2281cbff88611a422e
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-26 18:03:48 +0530

    Create alive.py

---
+++ mrjoker/modules/alive.py
@@ -0,0 +1,16 @@
+from telethon import events, Button, custom
+import re, os
+from mrjoker.events import register
+from mrjoker import telethn as tbot
+from mrjoker import telethn as tgbot
+PHOTO = "https://telegra.ph/file/007df1febcb6231298e47.jpg"
+@register(pattern=("/alive"))
+async def awake(event):
+  PIKACHU = event.sender.first_name
+  PIKACHU = "**🐺 I,m Wolf X** \n\n"
+  PIKACHU += "**🐺 I'm Working Properly**\n\n"
+  PIKACHU += "**🐺 Wolf X : 2.0 LATEST**\n\n"
+  PIKACHU += "**🐺 My Master :** [Hacker](t.me/HMF_OWNER_1)\n\n"
+  PIKACHU += "**🐺 Telethon Version : 1.23.0**\n\n"
+  BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/wolfxbotz"), Button.url("Copyrights©️", "https://t.me/Horimaya")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
