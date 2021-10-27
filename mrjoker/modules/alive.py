801c865edd55f613e41278db08f6762609e471e7
shukurenai0 <92837025+shukurenai0@users.noreply.github.com>
2021-10-27 20:02:27 +0530

    Update alive.py

--- mrjoker/modules/alive.py
+++ mrjoker/modules/alive.py
@@ -3,14 +3,14 @@ import re, os
 from mrjoker.events import register
 from mrjoker import telethn as tbot
 from mrjoker import telethn as tgbot
-PHOTO = "https://telegra.ph/file/c51b8dafd99ddf0bd30a6.jpg"
+PHOTO = "https://telegra.ph/file/c8ea5f5e21c09a0d2f5c9.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
-  PIKACHU = "**🐺 I,m Wolf X** \n\n"
-  PIKACHU += "**🐺 I'm Working Properly**\n\n"
-  PIKACHU += "**🐺 Wolf X : 2.0 LATEST**\n\n"
-  PIKACHU += "**🐺 My Master :** [Hacker](t.me/HMF_OWNER_1)\n\n"
-  PIKACHU += "**🐺 Telethon Version : 1.23.0**\n\n"
-  BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/wolfxbotz"), Button.url("Copyrights©️", "https://t.me/HMF_OWNER_1")]]
+  PIKACHU = "**😎 I,𝖒 𝖘𝖍𝖚 𝖐𝖚𝖗𝖊𝖓𝖆** \n\n"
+  PIKACHU += "**😎 I'm Working Properly**\n\n"
+  PIKACHU += "**😎 𝖘𝖍𝖚-𝖐𝖚𝖗𝖊𝖓𝖆 : 2.0 LATEST**\n\n"
+  PIKACHU += "** 😎 My Master :** [𝕯𝖊𝖊𝖕𝖆𝖐 𝖏𝖆𝖈𝖐](t.me/deepakjack007)\n\n"
+  PIKACHU += "**Telethon Version : 1.23.0**\n\n"
+  BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/shukurenairobot007"), Button.url("Copyrights©️", "https://t.me/deepakjack007")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
